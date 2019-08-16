f719d93ce415020e674e076a34fac3b4951bd50c
horse <littlehorseboy@gmail.com>
2019-08-16 13:42:00 +0800

    feat: redux-devtools-extension 會改變 createStore 回傳東西的型別 改回 redux-logger 並限制 dev 環境使用

--- src/reducers/configureStore.ts
+++ src/reducers/configureStore.ts
@@ -1,14 +1,28 @@
-import { createStore, combineReducers } from 'redux';
-// import { devToolsEnhancer } from 'redux-devtools-extension/developmentOnly';
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware,
+  Middleware,
+} from 'redux';
+import logger from 'redux-logger';
 import sceneObjectReducer from './sceneObject/sceneObject';
 
 const rootReducer = combineReducers({
   sceneObjectReducer,
 });
 
+let middlewares: Middleware[] = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares = [
+    ...middlewares,
+    logger,
+  ];
+}
+
 const store = createStore(
   rootReducer,
-  // devToolsEnhancer({}),
+  applyMiddleware(...middlewares),
 );
 
 export default store;
